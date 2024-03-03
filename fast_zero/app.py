55400ca2af4e275c8aee49c698f11d71596d1db5
azevedo <azevedoantoniocicero@gmail.com>
2024-03-03 16:07:39 -0300

    implementando o schema

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 
+from fast_zero.schemas import Message
+
+from fastapi.testclient import TestClient
+
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'mensage': 'Ola mundo'}
+
+
+cliente = TestClient(app)
+resposta = cliente.get('/')
+print(resposta)
