230f4113cc2133115ae3eccea83ffe4b74dd2b7c
azevedo <azevedoantoniocicero@gmail.com>
2024-03-04 10:20:08 -0300

    criando um endpoint que retorna um html e testando esse endpoint

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fast_zero.schemas import Message
 
@@ -7,4 +8,17 @@ app = FastAPI()
 
 @app.get('/', status_code=200, response_model=Message)
 def read_root():
-    return {'message': 'Ola mundo'}
+    return {'message': 'Olá mundo'}
+
+
+@app.get('/html', status_code=200, response_class=HTMLResponse)
+def read_root_html():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,9 +3,20 @@ from fastapi.testclient import TestClient
 from fast_zero.app import app
 
 
+def test_root_html_deve_retornar_200_e_ola_mundo():
+    client = TestClient(app)
+    texto = '<h1> Olá Mundo </h1>'
+
+    response = client.get('/html')
+
+    assert response.status_code == 200
+    assert texto in response.text
+
+
 def test_root_deve_retornar_200_e_ola_mundo():
     client = TestClient(app)
+
     response = client.get('/')
 
     assert response.status_code == 200
-    assert response.json() == {'message': 'Ola mundo'}
+    assert response.json() == {'message': 'Olá mundo'}
