23d804e3a50f7aa51a4fcaf464ee1f6bfb853b71
azevedo <azevedoantoniocicero@gmail.com>
2024-03-11 16:00:20 -0300

    Melhorando os testes e usando no banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+from fast_zero.schemas import UserPublic
+
+
 def test_create_user(client):
     response = client.post(
         '/users',
@@ -16,40 +19,57 @@ def test_create_user(client):
     }
 
 
+def test_error_create_user_existent(client, user):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'Test',
+            'email': 'test@test.com',
+            'password': 'testtest',
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'User already registered'}
+
+
 def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == 200
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'antonio',
-                'email': 'antonio@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_read_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+
+    assert response.status_code == 200
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_read_user_with_id(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
     response = client.get('/users/1')
 
     assert response.status_code == 200
-    assert response.json() == {
-        'username': 'antonio',
-        'email': 'antonio@example.com',
-        'id': 1,
-    }
+    assert response.json() == user_schema
 
 
-def teste_erro_read_user(client):
+def teste_erro_read_user_id_index_out_of_range(client):
     response = client.get('/users/0')
+
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User index out of range'}
+
+
+def teste_erro_read_user_not_found(client):
+    response = client.get('/users/2')
 
     assert response.status_code == 404
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -58,16 +78,13 @@ def test_update_user(client):
             'password': 'abc',
         },
     )
+    user_schema = UserPublic.model_validate(user).model_dump()
 
     assert response.status_code == 200
-    assert response.json() == {
-        'username': 'chapolin',
-        'email': 'chapolin@example.com',
-        'id': 1,
-    }
+    assert response.json() == user_schema
 
 
-def test_erro_update_user(client):
+def test_erro_update_user_id_index_out_of_range(client):
     response = client.put(
         '/users/0',
         json={
@@ -77,18 +94,38 @@ def test_erro_update_user(client):
         },
     )
     assert response.status_code == 404
+    assert response.json() == {'detail': 'User index out of range'}
+
+
+def test_erro_update_user_not_exist(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'chapolin',
+            'email': 'chapolin@example.com',
+            'password': 'abc',
+        },
+    )
+    assert response.status_code == 404
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == 200
     assert response.json() == {'message': 'User deleted'}
 
 
-def test_erro_delete_user(client):
+def test_erro_delete_user_index_out_of_range(client):
     response = client.delete('/users/0')
+
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User index out of range'}
+
+
+def test_erro_delete_user_not_found(client, user):
+    response = client.delete('/users/2')
 
     assert response.status_code == 404
     assert response.json() == {'detail': 'User not found'}
