c0007778b5ca2dddcc2ccd4f797dc553f9e74217
azevedo <azevedoantoniocicero@gmail.com>
2024-03-19 17:12:11 -0300

    Testando uma parada

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,13 +1,14 @@
 """API"""
 from fastapi import FastAPI
 
-from fast_zero.routes import auth, users
+from fast_zero.routes import auth, todos, users
 from fast_zero.schemas import Message
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todos.router)
 
 
 @app.get('/', status_code=200, response_model=Message)
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,17 +1,31 @@
 from sqlalchemy import select
+from sqlalchemy.orm import Session
 
-from fast_zero.models import User
+from fast_zero.models import Todo, User
 
 
-def test_create_user(session):
+def test_create_user(session: Session):
     new_user = User(username='antonio', password='1234', email='test@test')
     session.add(new_user)
     session.commit()
 
     user = session.scalar(select(User).where(User.username == 'antonio'))
 
-    assert (
-        (user.username == 'antonio')
-        and (user.password == '1234')
-        and (user.email == 'test@test')
+    assert user.username == 'antonio'
+
+
+def test_create_todo(session: Session, user: User):
+    todo = Todo(
+        title='Test Todo',
+        description='Test Desc',
+        state='draft',
+        user_id=user.id,
     )
+
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert todo in user.todos
